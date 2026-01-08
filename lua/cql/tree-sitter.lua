7560771b66379dc5665f40d9636ecf65953fa55b
wipermail <wipermail@gmail.com>
2026-01-08 23:53:44 +0300

    Refactor CQL parser registration in setup function

--- lua/cql/tree-sitter.lua
+++ lua/cql/tree-sitter.lua
@@ -2,19 +2,19 @@ local M = {}
 
 function M.setup()
   local parsers = require("nvim-treesitter.parsers")
-
- 	parsers.cql = {
- 		install_info = {
- 			url = "https://github.com/Akzestia/tree-sitter-cql",
- 			branch = "main",
- 			files = { "src/parser.c" },
- 			revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
- 			queries = "queries/neovim",
- 		},
- 		tier = 1,
-    filetype = "cqlang",
- 	}
-vim.treesitter.language.register("cql", "cql")
+  
+  parsers.cql = {
+    install_info = {
+   	  url = "https://github.com/Akzestia/tree-sitter-cql",
+   		branch = "main",
+   		files = { "src/parser.c" },
+   		revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
+   		queries = "queries/neovim",
+   	},
+   	tier = 1,
+    filetype = "cql",
+  }
+  vim.treesitter.language.register("cql", "cqlang")
 end
 
 return M
