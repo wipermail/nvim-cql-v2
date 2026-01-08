b862c5f09d6222bcce929ce816de5285390ef134
wipermail <wipermail@gmail.com>
2026-01-08 23:45:34 +0300

    Refactor CQL parser setup in tree-sitter.lua

--- lua/cql/tree-sitter.lua
+++ lua/cql/tree-sitter.lua
@@ -1,47 +1,20 @@
--- local M = {}
-
--- function M.setup()
--- 	local parsers = require("nvim-treesitter.parsers")
-
--- 	parsers.cql = {
--- 		install_info = {
--- 			url = "https://github.com/Akzestia/tree-sitter-cql",
--- 			branch = "main",
--- 			files = { "src/parser.c" },
--- 			revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
--- 			queries = "queries/neovim",
--- 		},
--- 		tier = 1,
--- 	}
-
--- 	vim.treesitter.language.register("cql", { "cqlang" })
-
--- 	vim.api.nvim_create_autocmd("FileType", {
--- 		pattern = "cqlang",
--- 		callback = function()
--- 			vim.treesitter.start()
--- 		end,
--- 	})
--- end
-
--- return M
-
 local M = {}
 
 function M.setup()
-  local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+  local parsers = require("nvim-treesitter.parsers")
 
-  parser_configs.cql = {
-    install_info = {
-      url = "https://github.com/Akzestia/tree-sitter-cql",
-      branch = "main",
-      files = { "src/parser.c" },
-      revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
-    },
+ 	parsers.cql = {
+ 		install_info = {
+ 			url = "https://github.com/Akzestia/tree-sitter-cql",
+ 			branch = "main",
+ 			files = { "src/parser.c" },
+ 			revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
+ 			queries = "queries/neovim",
+ 		},
+ 		tier = 1,
     filetype = "cqlang",
-  }
-
-  vim.treesitter.language.register("cql", "cqlang")
+ 	}
+vim.treesitter.language.register("cql", "cqlang")
 end
 
 return M
