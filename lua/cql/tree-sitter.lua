9fa35c74b80687f17ad7ddaf5daba08c2e991bf8
wipermail <wipermail@gmail.com>
2026-01-08 23:57:03 +0300

    2

--- lua/cql/tree-sitter.lua
+++ lua/cql/tree-sitter.lua
@@ -1,20 +1,19 @@
 local M = {}
 
-
 function M.setup()
-  local parsers = require("nvim-treesitter.parsers")
-  
-  parsers.cql = {
+  local parser_configs =
+    require("nvim-treesitter.parsers").get_parser_configs()
+
+  parser_configs.cql = {
     install_info = {
-   	  url = "https://github.com/Akzestia/tree-sitter-cql",
-   		branch = "main",
-   		files = { "src/parser.c" },
-   		revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
-   		queries = "queries/neovim",
-   	},
-   	tier = 1,
-    filetype = "cql",
+      url = "https://github.com/Akzestia/tree-sitter-cql",
+      branch = "main",
+      files = { "src/parser.c" },
+      revision = "b3fb29a60d096abf36726cfa2c2480e7c5ea777f",
+    },
+    filetype = "cqlang",
   }
+
   vim.treesitter.language.register("cql", "cqlang")
 end
 
